4a5fff49f9630cab5b3bb4278959ffe36ae02a3e
Arina <arinashkapa@yandex.ru>
2025-09-19 14:25:10 +0200

    created routers in app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,61 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />}>
+        <Route
+          path=':number'
+          element={
+            <Modal title='Детали заказа' onClose={() => {}}>
+              <OrderInfo />
+            </Modal>
+          }
+        />
+      </Route>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />}>
+        <Route path='orders' element={<ProfileOrders />} />
+        <Route
+          path='orders:number'
+          element={
+            <Modal title='' onClose={() => {}}>
+              <OrderInfo />
+            </Modal>
+          }
+        />
+      </Route>
+      <Route
+        path='/ingredients/:id'
+        element={
+          <Modal title='ингридиент' onClose={() => {}}>
+            <IngredientDetails />
+          </Modal>
+        }
+      />
+      <Route path='*' element={<NotFound404 />} />
+    </Routes>
   </div>
 );
-
 
 export default App;
